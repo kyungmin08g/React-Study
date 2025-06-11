7da4d1240182bc15aa17d0db83d8f45ddc0675c8
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-11 11:58:07 +0900

    [FEAT] Styled-components를 사용하여 CSS-in-JS 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import * as Style from './Style';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.ts</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Style.StyledDiv color='black'>테스트 글자</Style.StyledDiv>
   );
 }
-
-export default App;
