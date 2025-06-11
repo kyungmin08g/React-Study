806a484060f3f9f9845778c5ea262bd4cd11f7a5
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-11 21:29:26 +0900

    [UPDATE] Form 수정

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,18 +6,20 @@ export default function Login() {
   return (
     <div className="container-main">
       <div className="container">
-        <div className="login-title">
+        <form action={ "#" } method="GET">
+          <div className="login-title">
           <p className="title">Login</p>
-        </div>
-        <div className="input-components">
-          <Input type="text" message="아이디를 입력하세요." />
-        </div>
-        <div className="input-components">
-          <Input type="password" message="비밀번호를 입력하세요." />
-        </div>
-        <div className="button-box">
-          <Button title="Login"></Button>
-        </div>
+          </div>
+          <div className="input-components">
+            <Input type="text" message="아이디를 입력하세요." />
+          </div>
+          <div className="input-components">
+            <Input type="password" message="비밀번호를 입력하세요." />
+          </div>
+          <div className="button-box">
+            <Button title="Login"></Button>
+          </div>
+        </form>
       </div>
     </div>
   );
