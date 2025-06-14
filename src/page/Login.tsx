5ec0d35f891ffd792452dfa15ed95198f1dcc6c7
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-14 19:05:46 +0900

    [CHORE] useState, useEffect 설명 작성

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -6,6 +6,13 @@ import { useEffect, useState } from "react";
 export default function Login() {
   let [count, setCount] = useState(0);
 
+  /**
+   * useEffect()
+   * 해당 함수가 호출될때 같이 호출되는 녀석이다.
+   * 파라미터로는 콜백 함수와 리스트를 받는데 리스트는 선택이다. 리스트는 해당 리스트에 속해 있는 것의 상태가 변경될시 호출된다.
+   * 리스트를 입력하지 않으면 항상 해당 함수가 호출되면 같이 호출되는 Hook으로 된다.
+   * 형식: useEffect(() => {}, [상태 변경될 변수]);
+   */
   useEffect(() => {
     console.log("출력");
   }, [count]);
@@ -25,9 +32,7 @@ export default function Login() {
           </div>
           <div className="button-box">
             <Button title="Login" event={() => {
-              if (count === 0) {
-                setCount(++count);
-              }
+              if (count === 0) setCount(++count);
               console.log(`count: ${count}`);
             }} />
           </div>
--- src/page/SignIn.tsx
+++ src/page/SignIn.tsx
@@ -5,10 +5,23 @@ import { useParams } from "react-router-dom";
 import { useState } from "react";
 
 export default function SignIn() {
+  
+  /**
+   * useParams()
+   * URL에 담겨있는 파라미터를 얻는 Hook
+   */
   const { id, page } = useParams();
-  let [count, setCount] = useState(0);
-  console.log("체크");
 
+  /**
+   * useState()
+   * 상태가 변경될 수 있는 값을 다룰 수 있는 Hook(?)
+   * 형식: let [변수, 변경 함수] = useState(초기값);
+   * useState()는 상태가 변경되면 페이지를 재로딩한다.
+   */
+  let [count, setCount] = useState(0);
+
+  console.log("체크");
+  
   return (
     <div className="container-main">
       <div className="container">
