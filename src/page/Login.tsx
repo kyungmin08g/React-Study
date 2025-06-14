a096db6dc471b169de07bf1303f03fbf06aaac20
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-14 18:50:31 +0900

    [FEAT] useEffect hook 사용

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,8 +1,15 @@
 import "../styles/Login.css";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import { useEffect, useState } from "react";
 
 export default function Login() {
+  let [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("출력");
+  }, [count]);
+
   return (
     <div className="container-main">
       <div className="container">
@@ -17,7 +24,12 @@ export default function Login() {
             <Input type="password" message="비밀번호를 입력하세요." />
           </div>
           <div className="button-box">
-            <Button title="Login" event=""/>
+            <Button title="Login" event={() => {
+              if (count === 0) {
+                setCount(++count);
+              }
+              console.log(`count: ${count}`);
+            }} />
           </div>
         </form>
       </div>
