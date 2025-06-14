093ed3f7869c0ea892d42422b97d8ec1ae81e317
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-14 15:14:43 +0900

    [FEAT] React Router 사용하여 페이지 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import Login from "./components/Login";
+import SignIn from './components/SignIn';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={ <Login /> }></Route>
+          <Route path='/sign' element={ <SignIn /> }></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -2,13 +2,13 @@ import "../styles/Login.css";
 import Input from "./Input";
 import Button from "./Button";
 
-export default function Login() {
+export default function SignIn() {
   return (
     <div className="container-main">
       <div className="container">
         <form action={ "#" } method="GET">
           <div className="login-title">
-          <p className="title">Login</p>
+          <p className="title">Sign In</p>
           </div>
           <div className="input-components">
             <Input type="text" message="아이디를 입력하세요." />
@@ -17,7 +17,7 @@ export default function Login() {
             <Input type="password" message="비밀번호를 입력하세요." />
           </div>
           <div className="button-box">
-            <Button title="Login" />
+            <Button title="Sign In" />
           </div>
         </form>
       </div>
