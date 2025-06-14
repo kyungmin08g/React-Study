efcc2721f8eaa94b44681f1b5677800df0ead094
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-14 19:58:03 +0900

    [FEAT] useNavigate() 사용 및 설명 작성

--- src/page/SignIn.tsx
+++ src/page/SignIn.tsx
@@ -1,7 +1,7 @@
 import "../styles/Login.css";
 import Input from "../components/Input";
 import Button from "../components/Button";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useState } from "react";
 
 export default function SignIn() {
@@ -13,6 +13,12 @@ export default function SignIn() {
   const { id, page } = useParams();
 
   /**
+   * useNavigate()
+   * 뒤로가기/앞으로가기 등을 지원하는 Hook이다.
+   */
+  const navigate = useNavigate();
+
+  /**
    * useState()
    * 상태가 변경될 수 있는 값을 다룰 수 있는 Hook(?)
    * 형식: let [변수, 변경 함수] = useState(초기값);
@@ -21,7 +27,7 @@ export default function SignIn() {
   let [count, setCount] = useState(0);
 
   console.log("체크");
-  
+
   return (
     <div className="container-main">
       <div className="container">
@@ -47,6 +53,12 @@ export default function SignIn() {
           <p>카운트: { count }</p>
         </div> : <></>}
       </div>
+      <ul>
+          <li><button onClick={() => navigate(-2)}>뒤로 2번</button></li>
+          <li><button onClick={() => navigate(-1)}>뒤로 1번</button></li>
+          <li><button onClick={() => navigate(1)}>앞으로 1번</button></li>
+          <li><button onClick={() => navigate(2)}>앞으로 2번</button></li>
+      </ul>
     </div>
   );
 };
